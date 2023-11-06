3417f6e55ad8be5be217901d5fcfdc5e9da84aae
Landon Bouma <landonb@users.noreply.github.com>
2023-11-05 21:14:19 -0600

    Docs: Update header URLs

--- tests/test_defaults.py
+++ tests/test_defaults.py
@@ -1,5 +1,5 @@
 # Author: Landon Bouma <https://tallybark.com/>
-# Project: https://github.com/tallybark/easy-as-pypi-config#🍐
+# Project: https://github.com/doblabs/easy-as-pypi-config#🍐
 # Copyright © 2018-2020 Landon Bouma. All rights reserved.
 # License: MIT
 
